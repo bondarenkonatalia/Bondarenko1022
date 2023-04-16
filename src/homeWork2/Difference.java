d721cfbb9751e9d99ec07b04303bbd3f31be151c
natalia <bondarnata0866@gmail.com>
2023-04-16 18:34:20 +0300

    add new homWork3

--- src/homeWork2/Difference.java
+++ src/homeWork2/Difference.java
@@ -1,17 +1,18 @@
 package homeWork2;
 public class Difference {
-    public void razn() {
-        int[] mas = {23, 44, 2, 67, 5, 8, 21,4};
-        int a = 0;
-        int b = 0;
+    public void residual() {
+        int[] mas = {23, 44, 2, 67, 5, 8, 14, 4};
+
+        int even = 0;
+        int odd = 0;
         for (int i = 0; 1 < mas.length; i++) {
             if (i % 2 == 0) {
-                a += mas[i];
+                even += mas[i];
             } else {
-                b += mas[i];
+                odd += mas[i];
             }
         }
-        int differens = a - b;
+        int differens = even - odd;
         System.out.println("Разность между чётными и нечётными элементами равна: " + differens);
     }
 }
