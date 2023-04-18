aa19b16fc30a3518f01b6c096dc1a37168b8f518
natalia <bondarnata0866@gmail.com>
2023-04-18 14:50:12 +0300

    add new homWork3

--- src/Main.java
+++ src/Main.java
@@ -1,96 +1,9 @@
-import homeWork3.*;
+import homeWork2.Difference;
 
 public class Main {
-    /*public static void main(String[] args) {
-        Address address = new Address("Ivan","Petrov", 22, "M", new Address.UserAddress("RB","Vitebsk"));
-    }*/
-
-    //вызвать конструктор//
-     /*   User1 user11 = new User1("Anna", "Ivanova", 22, "W");
-        User1 user12 = new User1("Anna");
-        User1 user13 = new User1();
-    }*/
-
-  /*  public static void main(String[] args) {
-        USER user = USER.USER3;
-        user.println();
-        user.addAge(5);
-        user.printUser();
-    }
-}*/
-
-  /*  public static void main(String[] args) {
-
-        User user1 = new User();
-
-        user1.name = "Alla ";
-        user1.surname = "Petrova";
-        user1.age = 20;
-        user1.gender = "W";
-
-        user1.printFirstName();
-        user1.age();
-    }
-    User user = new User();
-}*/
-
     public static void main(String[] args) {
-        OverloadedMethod overloadedMethod = new OverloadedMethod();
-        overloadedMethod.sum(2,7,8);
-    }
-}
-   /* public static void main(String[] args) {
-        int[] arr = {6, 8, 3, 5, 1, 9, 24, 12,2};
-        Massif massif = new Massif();
-        massif.printArray(arr);
-        massif.maxvalue(arr);
-        massif.printelement(arr, 4);
 
-    }
-
-}*/
-
-    /*public static void main(String[] args) {
-
-        NewSeason newSeason = new NewSeason();
-        newSeason.NewSeason(MONTH.APRIL);
-    }
-}*/
-
-    /*public static void main(String[] args) {
-        Alphabet alfabet = new Alphabet();
-        alfabe
-        t.letter();*/
-
-    /*public static void main(String[] args) {
-        Seasons season = new Seasons();
-        season.Month();/*
-
-     /*public static void main(String[] args) {
-        Sum sum = new Sum();
-        sum.addition();*/
-
-   /*public static void main(String[] args) {
-        Words words = new Words();
-        words.Codeword();*/
-
-  /*  public static void main(String[] args) {
-        Fibonachi fibonachi = new Fibonachi();
-        int[] result = fibonachi.fib(6);
-        System.out.println(Arrays.toString(result));
-    }
-}*/
-
-    /*public static void main(String[] args) {
         Difference difference = new Difference();
-        difference.razn();
+        difference.residual();
     }
 }
-
-    /*public static void main(String[] args) {
-        SumElementov sumElementov = new SumElementov();
-        sumElementov.sloj();*/
-
-
-
-
